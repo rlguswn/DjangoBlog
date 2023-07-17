f595af0202714000800c879fd393cbde83687473
rlguswn <0318joo@naver.com>
2023-07-17 14:20:59 +0900

    글 수정 기능 생성

--- blog/urls.py
+++ blog/urls.py
@@ -10,9 +10,9 @@ urlpatterns = [
     # 글 상세 조회 /blog/detail/<int:pk>/
     path('detail/<int:pk>/', views.PostDetail.as_view(), name='detail'),
     # 글 작성 /blog/write/
-    # path('write/', views.PostWrite.as_view(), name='write'),
-    # 글 삭제 /blog/detail/edit/<int:pk>/
-    # path('detail/edit/<int:pk>/', views.PostEdit.as_view())
+    path('write/', views.PostWrite.as_view(), name='write'),
+    # 글 수정 /blog/detail/update/<int:pk>/
+    path('detail/update/<int:pk>/', views.PostUpdate.as_view(), name='update')
     # 글 삭제 /blog/detail/delete/<int:pk>/
-    # path('detail/delete/<int:pk>/', views.PostDelete.as_view())
+    # path('detail/delete/<int:pk>/', views.PostDelete.as_view(), name='delete')
 ]
--- blog/views.py
+++ blog/views.py
@@ -1,5 +1,6 @@
-from django.shortcuts import render
+from django.shortcuts import render, redirect
 from django.views import View
+from .forms import PostForm
 from .models import Post
 
 
@@ -8,7 +9,7 @@ class PostList(View):
     def get(self, request):
         posts = Post.objects.all()
         context = {
-            'posts': posts,
+            'posts': posts
         }
         return render(request, 'blog/post_list.html', context)
 
@@ -20,3 +21,37 @@ class PostDetail(View):
             'post': post
         }
         return render(request, 'blog/post_detail.html', context)
+
+
+class PostWrite(View):
+    def get(self, request):
+        form = PostForm()
+        context = {
+            'form': form
+        }
+        return render(request, 'blog/post_form.html', context)
+    def post(self, request):
+        form = PostForm(request.POST)
+        if form.is_valid():
+            post = form.save()
+            return redirect('blog:list')
+
+
+class PostUpdate(View):
+    def get(self, request, pk):
+        post = Post.objects.get(pk=pk)
+        form = PostForm(initial={'title':post.title, 'content':post.content})
+        context = {
+            'form': form,
+            'post': post
+        }
+        return render(request, 'blog/post_edit.html', context)
+
+    def post(self, request, pk):
+        post = Post.objects.get(pk=pk)
+        form = PostForm(request.POST)
+        if form.is_valid():
+            post.title = form.cleaned_data['title']
+            post.content = form.cleaned_data['content']
+            post.save()
+            return redirect('blog:detail', pk=pk)
